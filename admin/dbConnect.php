6498bc4398b8690227cbca809dfdcb406559a155
Sid Malde <smalde@ssl247.co.uk>
2016-11-03 14:52:30 +0000

    update

--- admin/dbConnect.php
+++ admin/dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 	// connect to MySQL Server
-	$connection = mysqli_connect("127.0.0.1", "sidmalde", "El3phant", "visaline_visaline")
+	$connection = mysqli_connect("127.0.0.1", "sidmalde", "F3rrari1", "visaline_visaline")
 	or die('Could not connect: ' . mysql_error());
 	
 	// select db
